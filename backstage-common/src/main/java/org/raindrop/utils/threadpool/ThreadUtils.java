122b69c1228ae6f25bdfaf2d77be12298d357acb
Raindrop <635880154@qq.com>
2022-04-18 15:04:43 +0800

    add(common): 添加线程池工具类

--- backstage-common/src/main/java/org/raindrop/utils/threadpool/ThreadUtils.java
+++ backstage-common/src/main/java/org/raindrop/utils/threadpool/ThreadUtils.java
@@ -5,8 +5,8 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.util.Map;
 import java.util.concurrent.*;
-import java.util.function.Function;
 
+@Slf4j
 public class ThreadUtils {
     /**
      * 线程池以及配置信息
@@ -44,15 +44,15 @@ public class ThreadUtils {
      *
      * @return
      */
-    public static ExecutorService createPools(String threadNamePrefix) {
+    private static ExecutorService createPools(String threadNamePrefix) {
         return createPools(threadNamePrefix, new ThreadConfig(), false);
     }
 
-    public static ExecutorService createPools(String threadNamePrefix, boolean daemon) {
+    private static ExecutorService createPools(String threadNamePrefix, boolean daemon) {
         return createPools(threadNamePrefix, new ThreadConfig(), daemon);
     }
 
-    public static ExecutorService createPools(String threadPoolName, ThreadConfig threadConfig, boolean daemon) {
+    private static ExecutorService createPools(String threadPoolName, ThreadConfig threadConfig, boolean daemon) {
         ThreadFactory threadFactory = createThreadFactory(threadPoolName, daemon);
 
         ExecutorService threadPool = new ThreadPoolExecutor(threadConfig.getCorePoolSize(), threadConfig.getMaximumPoolSize(),
@@ -69,7 +69,7 @@ public class ThreadUtils {
      * @param daemon         是否守护线程
      * @return
      */
-    public static ThreadFactory createThreadFactory(String threadPoolName, boolean daemon) {
+    private static ThreadFactory createThreadFactory(String threadPoolName, boolean daemon) {
         if (threadPoolName != null) {
             return new ThreadFactoryBuilder()
                     .setNameFormat(threadPoolName + "-%d")
@@ -79,8 +79,38 @@ public class ThreadUtils {
         return Executors.defaultThreadFactory();
     }
 
-    public static void main(String[] args) {
-        // threadPool.isShutdown() || threadPool.isTerminated() 如果线程池在这两种状态下，执行任务会出现什么情况
-        ExecutorService executorService = Executors.newFixedThreadPool(1);
+    /**
+     * shutDown 所有线程池
+     */
+    public static void shutDownAllThreadPool() {
+        log.info("call shutDownAllThreadPool method");
+        threadPools.entrySet().parallelStream().forEach(entry -> {
+            ExecutorService executorService = entry.getValue();
+            executorService.shutdown();
+            log.info("shut down thread pool [{}] [{}]", entry.getKey(), executorService.isTerminated());
+            try {
+                executorService.awaitTermination(10, TimeUnit.SECONDS);
+            } catch (InterruptedException e) {
+                log.error("Thread pool never terminated");
+                executorService.shutdownNow();
+            }
+        });
+    }
+
+    /**
+     * 打印线程池的状态
+     *
+     * @param threadPool 线程池对象
+     */
+    public static void printThreadPoolStatus(ThreadPoolExecutor threadPool) {
+        ScheduledExecutorService scheduledExecutorService = new ScheduledThreadPoolExecutor(1, createThreadFactory("print-thread-pool-status", false));
+        scheduledExecutorService.scheduleAtFixedRate(() -> {
+            log.info("============ThreadPool Status=============");
+            log.info("ThreadPool Size: [{}]", threadPool.getPoolSize());
+            log.info("Active Threads: [{}]", threadPool.getActiveCount());
+            log.info("Number of Tasks : [{}]", threadPool.getCompletedTaskCount());
+            log.info("Number of Tasks in Queue: {}", threadPool.getQueue().size());
+            log.info("===========================================");
+        }, 0, 1, TimeUnit.SECONDS);
     }
 }
