64ab219b06400c1fe7eea8451ec58eb6cd774b85
Raindrop <635880154@qq.com>
2022-05-24 03:06:27 +0800

    feat：xxl-job

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # 这是Backstage的项目升级版本
 - 升级为SpringCould框架
 - 使用nacos作为配置中心
-- 使用Shrio做分布式认证中心
-- 使用Spring Gateway作为网管
+- 使用Security做分布式认证中心
+- 使用Spring Gateway作为网关
+
+# Server Port
+- 系统服务 1000-2000
+- security 服务 6666
+- 网关服务 9999
+- 其他微服务 port 0 
--- backstage-common/backstage-common-base/src/main/java/org/raindrop/common/handle/GlobalBizExceptionHandler.java
+++ backstage-common/backstage-common-base/src/main/java/org/raindrop/common/handle/GlobalBizExceptionHandler.java
@@ -9,7 +9,6 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
-import java.nio.file.AccessDeniedException;
 import java.util.List;
 
 /**
